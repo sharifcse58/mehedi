9e68cfd238f65fa0c044bff67c39f2f2d06c5c26
sharifcse57 <you@example.com>
2025-01-22 03:59:32 +0600

    github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
