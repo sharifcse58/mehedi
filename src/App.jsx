2f0344766cf9aefda59a390a90dea3c30ebe41c1
sharifcse57 <you@example.com>
2025-01-22 03:21:26 +0600

    Update build file add

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,35 @@ import Blog from "./pages/Blog";
 import About from "./pages/About";
 import RecentBlogs from "./pages/RecentBlogs";
 import BlogDetails from "./pages/BlogDetails";
+import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
 
 const App = () => {
   return (
     <div className="w-full min-h-screen bg-black text-white flex flex-col">
-      <Router>
-        <Header />
-        <div className="flex-grow">
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Home />
-                  <RecentBlogs />
-                </>
-              }
-            />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/blog/:id" element={<BlogDetails />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
+      <BrowserRouter basename="/mehedi">
+        <Router>
+          <Header />
+          <div className="flex-grow">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Home />
+                    <RecentBlogs />
+                  </>
+                }
+              />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/blog/:id" element={<BlogDetails />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </BrowserRouter>
     </div>
   );
 };
